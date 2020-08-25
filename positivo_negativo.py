1e40693d3dc15c73db4fcbfcf87337a3e2442d43
Michel Pinho <69489013+MichelPinho@users.noreply.github.com>
2020-08-25 20:13:41 -0300

    Add files via upload

---
+++ positivo_negativo.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+num1 = int(input(' Informe um número :'))
+##num2 = int(input(' Informe o segundo número :'))
+
+if num1 > 0:
+    print(f' O seu número é POSITIVO')
+
+else:
+    print(f' O seu número é NEGATIVO')
