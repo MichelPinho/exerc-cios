d32e68085dc2a5103cf6eb638ce5bbc5551d0620
Michel Pinho <69489013+MichelPinho@users.noreply.github.com>
2020-08-25 17:26:34 -0300

    Add files via upload

---
+++ graus_celsius_para_fahrenheit.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+num1 = float(input(' Digite aqui a temperatura em graus Celsius "C" : '))
+
+conv = float (((9*num1)+160)/5)
+
+print(' A temperatura em graus Fahrenheit "F" é : %s' % conv)
+
