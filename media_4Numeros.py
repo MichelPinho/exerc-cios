5e54498e9ac8f7d4489c1350e71e55b58c1fdd84
Michel Pinho <69489013+MichelPinho@users.noreply.github.com>
2020-08-25 17:20:46 -0300

    Add files via upload

---
+++ media_4Numeros.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+num1 = int(input(' Digite a primeira nota : '))
+num2 = int(input(' Digite a segunda nota : '))
+num3 = int(input(' Digite a terceira nota : '))
+num4 = int(input(' Digite a quarta nota : '))
+
+media = int((num1 + num2 + num3 + num4) / 4)
+print(' A média das suas notas são : %s' % media)
+
