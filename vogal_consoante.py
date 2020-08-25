6877127aa0b745413e516c6af85e0cc46ddf1a61
Michel Pinho <69489013+MichelPinho@users.noreply.github.com>
2020-08-25 20:16:32 -0300

    Add files via upload

---
+++ vogal_consoante.py
@@ -0,0 +1,9 @@
+#aça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+acao = str(input(' Informe a letra que deseja descobrir :'))
+
+if acao == 'a' or acao == 'e' or acao == 'i' or acao == 'o' or acao == 'u':
+    print('É vogal')
+
+else:
+    print('É consoante')
