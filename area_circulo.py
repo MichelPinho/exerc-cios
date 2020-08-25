0351e4e7b2ca8cabc88a800e0faec9d5847f2a95
Michel Pinho <69489013+MichelPinho@users.noreply.github.com>
2020-08-25 17:22:01 -0300

    Add files via upload

---
+++ area_circulo.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+num1 = int(input(' Digite aqui a sua medida do raio do círculo : '))
+
+conv = float (3.14 * (num1 ** 2))
+
+print(' A área do círculo é : %s' % conv)
+
