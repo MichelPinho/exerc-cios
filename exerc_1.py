7dc87ce1a947814f1b83cb9fa36ea96f56d5f385
Michel Pinho <69489013+MichelPinho@users.noreply.github.com>
2020-08-25 16:38:53 -0300

    Add files via upload

--- exerc_1.py
+++ exerc_1.py
@@ -1,52 +1,20 @@
-```##	Faça um programa que leia e valide as seguintes informações:
-##	Nome: maior que 3 caracteres;
-##	Idade: entre 0 e 150;
-##	Salário: maior que zero;
-##	Sexo: 'f' ou 'm';
-##	Estado Civil: 's', 'c', 'v', 'd';
-## utilizando somente while ou for.
+```
+##Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+##	o produto do dobro do primeiro com metade do segundo .
+##	a soma do triplo do primeiro com o terceiro.
+##	o terceiro elevado ao cubo.
 
-nome = str(input('Digite seu nome:'))
+num1 = int(input(' Digite aqui um número inteiro : '))
+num2 = int(input(' Digite aqui um número inteiro  : '))
+num3 = float(input(' Digite aqui um número real : '))
 
-while len(nome) < 3:
-    print('nome inválido')
-    str(input('Digite seu nome:'))
-else:
-    print('Nome validado')
+conv1 = float ((num1 * 2) * (num2/2))
+conv2 = int ((num1 * 3) + num3)
+conv3 = float (num3 ** 3)
+print(' O produto do dobro do primeiro com metade do segundo é : %s' % conv1)
+print(' A soma do triplo do primeiro com o terceiro é : %s ' % conv2)
+print(' O terceiro elevado ao cubo é : %s' % conv3)
 
-idade = int(input('Digite a sua idade :'))
-
-while idade < 0 or idade > 150:
-    print('Idade inválida')
-    input('Digite a sua idade :')
-else:
-    print('Idade validada')
-
-salario = float(input('Informe seu salário : '))
-
-while salario < 0:
-    print('ERRO: salário menor que 0, refaça a operação')
-    (float('Informe seu salário : '))
-else:
-    print('Salário válido')
-
-sexo = input('Informe seu sexo (M) ou (F): ').lower()
-
-while sexo != 'm' and 'f':
-    print(' ERRO: sexo inválido, repita a operação')
-    input('Informe seu sexo (M) ou (F): ').lower()
-else:
-    print('Sexo validado')
-
-estado = input('Informe seu estado civil casado (C), solteiro(S), viúvo (V) ou disquitado (D):').lower()
-
-while estado != 'c' and estado != 's' and estado != 'v' and estado != 'd':
-    print('ERRO: Estado civil inválido, repita a operação')
-    input('Informe seu estado civil casado (C), solteiro(S), viúvo (V) ou disquitado (D):')
-
-else:
-    print('Estado civil validado')
-    print('\nObrigado')
 
 ```
 
