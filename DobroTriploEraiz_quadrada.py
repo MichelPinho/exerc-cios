44a4e1ffe01031f921e1195e221d8c0d3a6dafe2
Michel Pinho <jologpinho@gmail.com>
2020-09-13 15:06:58 -0300

    Add files via upload

---
+++ DobroTriploEraiz_quadrada.py
@@ -0,0 +1,8 @@
+num = int(input(' Digite um número : '))
+
+dobro = num * 2
+triplo = num * 3
+raiz = num * (1/2)
+
+print(f' O dobro do número é {dobro}.\n O triplo desse número é {triplo}.\n A raiz quadrada é {raiz}.')
+
