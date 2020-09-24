b7be02f1c8f62b46237a3d62c271b40bf69ff124
Michel Pinho <jologpinho@gmail.com>
2020-09-24 16:10:48 -0300

    Add files via upload

---
+++ Fogos_de_Artifício.py
@@ -0,0 +1,8 @@
+# CRIE UM PROGRAMA QUE MOSTRE NA TELA UMA CONTAGEM REGRESSIVA PARA O ESTOURO DE FOGOS DE ARTIFÍCIO ,
+#INDO DE 10 ATÉ 0. COM UMA PAUSA DE 1 SEGUNDO ENTRE ELES.
+from time import sleep
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('BOOM   BOOM    POOW !!')
+
