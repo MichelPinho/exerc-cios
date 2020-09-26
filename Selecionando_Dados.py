4fd923193b6f3a6eecbe3fd1f360040f6875c6db
Michel Pinho <jologpinho@gmail.com>
2020-09-25 21:13:33 -0300

    Add files via upload

---
+++ Selecionando_Dados.py
@@ -0,0 +1,28 @@
+# FAÇA UM PROGRAMA QUE LEIA A IDADE, NOME E SEXO DE 4 PESSOAS, NO FINAL MOSTRE:
+# 1 - A MÉDIA DA IDADE DO GRUPO
+# 2 - QUAL É O NOME DO HOMEM MAIS VELHO
+# 3 - QUANTAS MULHERES TEM MENOS DE 20 ANOS.
+soma = 0
+media = 0
+maioridadehomem = 0
+nomevelho = 0
+totmulher20 = 0
+for p in range(1, 5):
+    nome = str(input('Informe seu nome: ')).strip()
+    idade = int(input('Informe a sua idade: '))
+    sexo = str(input('Informe seu sexo [M/F]: ')).strip()
+    soma += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+
+
+media = soma / 4
+print(f'A media é {media}')
+print(f'O homem mais velho se chama {nomevelho} e tem {maioridadehomem} anos.')
+print(f'Ao todo são {totmulher20} mulheres com menos de 20 anos.')
