6b2fe77269b08fb1b7e06bb58345f69015ef51cc
Michel Pinho <jologpinho@gmail.com>
2020-10-02 17:17:35 -0300

    Calculando PA com while
    aprimorado o exercício de como calcular uma PA , termos e razões usando while .

--- Calculando_PA_While.py
+++ Calculando_PA_While.py
@@ -3,8 +3,15 @@ primeiro = int(input('Informe o primeiro termo da razão de uma P.A. ->  '))
 razao = int(input('Informe a razão dessa P.A. ->  '))
 termo = primeiro
 cont = 1
-while cont <= 10:
-    print('{} -> '.format(termo), end='')
-    termo += razao
-    cont += 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('PAUSE')
+    mais = int(input('Quantos termos você quer mostrar mais ?'))
+print(f'\033[32mProgressão finalizada com sucesso, foram utilizados {total} termos neste exercício.')
 print('FIM')
