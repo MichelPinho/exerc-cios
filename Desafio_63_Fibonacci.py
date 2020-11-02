b9a668ad14b717f10340c6a38e6e3ee93b1be801
Michel Pinho <jologpinho@gmail.com>
2020-11-02 20:47:51 -0300

    Desafio 63
    Feito exercício para programar uma sequencia de Fibonacci

--- Desafio_63_Fibonacci.py
+++ Desafio_63_Fibonacci.py
@@ -12,8 +12,9 @@ print('{} - {}'.format(t1, t2), end='')
 cont = 3
 while cont <= n:
    t3 = t1 + t2
-   print('- {}'.format(t3), end='')
+   print(' - {}'.format(t3), end='')
    t1 = t2
    t2 = t3
    cont += 1
 print('- FIM')
+7
