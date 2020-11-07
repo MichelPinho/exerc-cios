c64ad699d28160e7a296fc68dd2a180797d6f979
Michel Pinho <jologpinho@gmail.com>
2020-11-07 19:01:52 -0300

    Create Desafio_66_OperacãoNúmeros.py

---
+++ Desafio_66_OperacãoNúmeros.py
@@ -0,0 +1,15 @@
+# Crie um programa que leia números inteiros pelo teclado. 
+# O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. 
+# No final, mostre quantos números foram digitados e qual foi a soma entre elas 
+# (desconsiderando o flag).
+
+cont = soma = 0
+while True:
+    num = int(input('Digite um valor [999 encerra o programa] ->  '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+
+print(f'A quantidade de números digitados foram {cont} e a soma deles é {soma}.')
+
